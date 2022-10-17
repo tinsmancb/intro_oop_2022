d07664fcfd1b5124eaa95000e72977f9b420f8b4
ctinsman <ctinsman@thegraysonschool.org>
2022-10-17 14:01:08 -0400

    Correct typo in main.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
     print(f"The point ({p1.x}, {p1.y}) is at a distance of {p1.abs()} from the origin.")
 
     p2 = Point(-1.0, 6.5)
-    print(f"It is a distance {p1.dist_do(p2):.5} away from the point ({p2.x}, {p2.y}).")
+    print(f"It is a distance {p1.dist_to(p2):.5} away from the point ({p2.x}, {p2.y}).")
 
 
 if __name__ == "__main__":
